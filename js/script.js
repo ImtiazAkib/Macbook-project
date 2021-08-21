064582f87f8fdf6e18be8ddeffac54c97a497d62
ImtiazAkib <imtiazakib007@gmail.com>
2021-08-21 13:28:53 +0600

    new

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('btn-16gb-memory').addEventListener('click', function () {
+    const memoryCost = document.getElementById('memo-cost');
+    let memoryCostValue = memoryCost.innerText;
+    memoryCostValue = 180;
+})
