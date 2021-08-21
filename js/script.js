e933698696eb33fe118d3de064ccbb5d77278428
ImtiazAkib <imtiazakib007@gmail.com>
2021-08-21 13:44:34 +0600

    new

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-document.getElementById('btn-16gb-memory').addEventListener('click', function () {
+function memoryCostCalc(memorySize) {
     const memoryCost = document.getElementById('memo-cost');
-    let memoryCostValue = memoryCost.innerText;
-    memoryCostValue = 180;
+    if (memorySize == '8gb') {
+        memoryCost.innerText = 0;
+    }
+    else if (memorySize == '16gb') {
+        memoryCost.innerText = 180;
+    }
+}
+document.getElementById('btn-primary-memory').addEventListener('click', function () {
+
+    memoryCostCalc('16gb');
+
 })
+document.getElementById('btn-base-memory').addEventListener('click', function () {
+    memoryCostCalc('8gb');
+
+})
+
+// storage calculation
+
